928e9d0063263cb5c55236c2c1f1e0e2c8434577
PavloLevchenko <teasetrnet@gmail.com>
2022-02-12 18:22:00 +0200

    Меняет ContactItem на функциональный тип

--- src/ContactItem/ContactItem.js
+++ src/ContactItem/ContactItem.js
@@ -1,22 +1,19 @@
-import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import s from './ContactItem.module.css';
 
-class ContactItem extends Component {
-  static propTypes = {
-    name: PropTypes.string.isRequired,
-    number: PropTypes.string.isRequired,
-  };
-  render() {
-    const { name, number, onDeleteClick } = this.props;
-    return (
-      <li className={s.contactItem}>
-        {name} : {number}
-        <button className={s.btnDelete} type="button" onClick={onDeleteClick}>
-          Delete
-        </button>
-      </li>
-    );
-  }
-}
+const ContactItem = ({ name, number, onDeleteClick }) => (
+  <li className={s.contactItem}>
+    {name} : {number}
+    <button className={s.btnDelete} type="button" onClick={onDeleteClick}>
+      Delete
+    </button>
+  </li>
+);
+
+ContactItem.propTypes = {
+  name: PropTypes.string.isRequired,
+  number: PropTypes.string.isRequired,
+  onDeleteClick: PropTypes.func.isRequired,
+};
+
 export default ContactItem;
